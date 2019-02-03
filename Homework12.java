da532cf70f29c95fe7010cdb459d8beb2bfaa479
Abby Forman <formaabb@berkeleyprep.org>
2019-02-03 16:59:44 -0500

    Abby Forman

--- Homework12.java
+++ Homework12.java
@@ -7,18 +7,20 @@ import java.util.ArrayList;
 
 public class Homework12 {
 
+	private ArrayList<RectangularShape> arr;
+
 	/* Finish the constructor and create any necessary instance
 	 * variables. The constructor should create and save an
 	 * ArrayList of RectangularShape values
 	 */
 	public Homework12() {
-
+		arr = new ArrayList<RectangularShape>();
 	}
 
 	/* Adds the parameter s to the ArrayList
 	 */
 	public void addShape(RectangularShape s) {
-
+		arr.add(s);
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -26,7 +28,13 @@ public class Homework12 {
 	 * parameter r
 	 */
 	public int problem1(Rectangle2D r) {
-
+		int counter = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i).intersects(r)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -34,33 +42,60 @@ public class Homework12 {
 	 * parameter p
 	 */
 	public int problem2(Point2D p) {
-
+		int counter = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (!arr.get(i).contains(p)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 
 	/* Returns the number of Ellipse2D objects that
 	 * are in the ArrayList
 	 */
 	public int problem3() {
-
+		int counter = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i) instanceof Ellipse2D) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 
 	/* Returns true if any of the Rectangle2D objects
 	 * in the ArrayList intersects the parameter Line2D
 	 */
 	public boolean problem4(Line2D l) {
-
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i) instanceof Rectangle2D) {
+				if (l.intersects((Rectangle2D) arr.get(i))) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	/* Return the total area of the RectangularShape objects in
 	 * the AraryList
 	 * For this problem, you may assume that the only shapes
-	 * that have area are rectangles and ellipses
+	 * that have area are arrs and ellipses
 	 * Note that not all ellipses are circles
 	 * If two objects intersect, the intersected area
 	 * counts for both (i.e. do not account for intersection)
 	 */
 	public double problem5() {
-
+		double total = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i) instanceof Rectangle2D) {
+				total += arr.get(i).getWidth() * arr.get(i).getHeight();
+			} else if (arr.get(i) instanceof Ellipse2D) {
+				total += (arr.get(i).getWidth()/2) * (arr.get(i).getHeight()/2) * Math.PI;
+			}
+		}
+		return total;
 	}
 
 	public static void main(String[] args) {
